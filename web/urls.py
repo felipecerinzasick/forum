ff95dc968aa9429d6d2fd875c77169c4a81d5ef4
keval kant <kevalkant9098@gmail.com>
2021-03-09 23:22:28 +0530

    changes

--- web/urls.py
+++ web/urls.py
@@ -37,6 +37,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+                              document_root=settings.MEDIA_ROOT)
 
     path('', include('blog.urls')),
