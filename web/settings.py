110c183d6d95e10e6bce4c800c448bd4593e3393
keval kant <kevalkant9098@gmail.com>
2021-03-09 23:07:33 +0530

    changes

--- web/settings.py
+++ web/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
+    
 ]
 
 MIDDLEWARE = [
@@ -131,19 +133,27 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-cdn')
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles') 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'web/static')
-]
-
-
-
-# Media Files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
+
+AWS_ACCESS_KEY_ID = 'AKIAYL2ZDYLEHVZRVMZP'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'sick-leads'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+DEFAULT_FILE_STORAGE = 'web.storage_backends.MediaStorage'
 
 # Crispy Settings
 
