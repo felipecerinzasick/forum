38db24a3aa0c5e5fa125e1f83a760e96bae7c915
keval kant <kevalkant9098@gmail.com>
2021-03-10 12:30:41 +0530

    changes

--- users/models.py
+++ users/models.py
@@ -13,9 +13,9 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        img = Image.open(self.image.url)
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            img.save(self.image.url)
