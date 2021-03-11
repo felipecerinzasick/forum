cbdf8542f5833c691aa8f57ff6076d4c7b5c4286
sickventures <69673432+sickventures@users.noreply.github.com>
2021-03-11 11:16:53 +0100

    compress enabled

--- web/settings.py
+++ web/settings.py
@@ -163,7 +163,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
